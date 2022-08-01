cda8b66bbad16a7bd362a31424e4a17c627c047a
SeanChen <abbb123p@gmail.com>
2022-08-01 16:43:01 +0800

    update CRUD done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+
+  private
+  def record_not_found
+    # redirect_to "/"
+    render file: "#{Rails.root}/public/404.html", layout: false, status: 404
+    #layout: false 不要再套layout裡面的外殼，並且status 告訴瀏覽器是錯誤404
+  end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,37 @@
 class ArticlesController < ApplicationController
+
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
+  # before_action :find_article, except: [:create] #如果有加新的action會跟著執行，上面比較明確規範
+
+  def destroy
+    @article.destroy
+    redirect_to blogs_path 
+
+  end
+
+  def update 
+    if @article.update(article_params)
+      redirect_to blogs_path
+    else
+      render :edit
+      # render "/articles/edit"
+      # redirect_to edit_article_path(@article)
+    end
+  end
+  
+  def edit
+  end
+  
+  def show 
+    # Article.find(3) #find直接接id，找不到會直接出錯activeRecord::RecordNotFound，建議使用這種比較激進
+    # Article.find_by(id: 3) #find_by可以接id以外的東西，找不到會得到nil
+  end
+  
   def create
     # clean_params = params.require(:article).permit(:title, :content)
     # clean_params = params[:article].permit(:title, :content)
     # 避免forbbidon attributes
-    
     @article = Article.new(article_params)
-    
     if @article.save
       redirect_to "/"
     else
@@ -18,6 +44,10 @@ class ArticlesController < ApplicationController
   private
   def article_params
     params.require(:article).permit(:title, :content)
+  end
+
+  def find_article
+    @article = Article.find(params[:id]) 
   end
 
 end
